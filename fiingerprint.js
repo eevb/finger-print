6dbae460bf2512d0a3fd5226077f4380eecdcdf3
eevb <96337431+eevb@users.noreply.github.com>
2023-06-02 12:26:59 +0200

    devtools detect

--- fiingerprint.js
+++ fiingerprint.js
@@ -1,109 +1,109 @@
-function generate(){
+function generate() {
 
-// Get the domain name from the current URL
-const domainName = window.location.origin;
+  // Get the domain name from the current URL
+  const domainName = window.location.origin;
 
-// Generate a random hexadecimal string
-function generateRandomHex(length) {
-  let result = '';
-  const characters = '0123456789abcdef';
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  // Generate a random hexadecimal string
+  function generateRandomHex(length) {
+    let result = '';
+    const characters = '0123456789abcdef';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
   }
-  return result;
-}
 
-// Replace numbers and letters with random hexadecimal values every 100 milliseconds
-const intervalId = setInterval(() => {
-  const hexValue = generateRandomHex(40);
-  const newURL = `${domainName}/${hexValue}`;
-  window.history.pushState({}, document.title, newURL);
-  document.getElementById("FPsum").textContent = hexValue;
-}, 100);
+  // Replace numbers and letters with random hexadecimal values every 100 milliseconds
+  const intervalId = setInterval(() => {
+    const hexValue = generateRandomHex(40);
+    const newURL = `${domainName}/${hexValue}`;
+    window.history.pushState({}, document.title, newURL);
+    document.getElementById("FPsum").textContent = hexValue;
+  }, 100);
 
-// Clear the URL and HTML element after 2 seconds
-setTimeout(() => {
-  clearInterval(intervalId);
-  window.history.pushState({}, document.title, domainName);
-  document.getElementById("FPsum").textContent = "";
-}, 2000);
+  // Clear the URL and HTML element after 2 seconds
+  setTimeout(() => {
+    clearInterval(intervalId);
+    window.history.pushState({}, document.title, domainName);
+    document.getElementById("FPsum").textContent = "";
+  }, 2000);
 
 
-function changeurl() {
+  function changeurl() {
 
-  let newURL = '/finger-print/';
+    let newURL = '/finger-print/';
 
-  // Change the URL back to normal
-  window.history.pushState({}, '', newURL);
+    // Change the URL back to normal
+    window.history.pushState({}, '', newURL);
 
-}
-//----------------------------------------------------------------
+  }
+  //----------------------------------------------------------------
 
-// Delayed script
-function delayedScript() {
+  // Delayed script
+  function delayedScript() {
 
-  changeurl()
+    changeurl()
 
-  async function calculateBrowserFingerprint() {
-    // User Agent
-    const userAgent = navigator.userAgent;
+    async function calculateBrowserFingerprint() {
+      // User Agent
+      const userAgent = navigator.userAgent;
 
-    // Screen Properties
-    const screenWidth = window.screen.width;
-    const screenHeight = window.screen.height;
-    const screenColorDepth = window.screen.colorDepth;
+      // Screen Properties
+      const screenWidth = window.screen.width;
+      const screenHeight = window.screen.height;
+      const screenColorDepth = window.screen.colorDepth;
 
-    // Timezone Offset
-    const timezoneOffset = new Date().getTimezoneOffset();
+      // Timezone Offset
+      const timezoneOffset = new Date().getTimezoneOffset();
 
-    // Available Fonts
-    const availableFonts = [];
-    const fontList = ['Arial', 'Arial Black', 'Arial Unicode MS', 'Courier New', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana'];
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
+      // Available Fonts
+      const availableFonts = [];
+      const fontList = ['Arial', 'Arial Black', 'Arial Unicode MS', 'Courier New', 'Georgia', 'Impact', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana'];
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
 
-    for (let i = 0; i < fontList.length; i++) {
-      ctx.font = "72px sans-serif";
-      ctx.fillText('a', 0, 100);
-      ctx.font = "72px '" + fontList[i] + "', sans-serif";
-      ctx.fillText('a', 0, 200);
-      const data = ctx.getImageData(0, 0, 1, 1).data;
-      if (data[3] !== 0) {
-        availableFonts.push(fontList[i]);
+      for (let i = 0; i < fontList.length; i++) {
+        ctx.font = "72px sans-serif";
+        ctx.fillText('a', 0, 100);
+        ctx.font = "72px '" + fontList[i] + "', sans-serif";
+        ctx.fillText('a', 0, 200);
+        const data = ctx.getImageData(0, 0, 1, 1).data;
+        if (data[3] !== 0) {
+          availableFonts.push(fontList[i]);
+        }
       }
+
+      // Language
+      const language = navigator.language;
+
+      // Hardware Concurrency
+      const hardwareConcurrency = navigator.hardwareConcurrency;
+
+      // Concatenate fingerprint components
+      const fingerprint = userAgent + screenWidth + screenHeight + screenColorDepth + timezoneOffset + availableFonts.join(',') + language + hardwareConcurrency;
+
+      // Hash the fingerprint using SHA-1
+      const encoder = new TextEncoder();
+      const data = encoder.encode(fingerprint);
+      const hashBuffer = await crypto.subtle.digest('SHA-1', data);
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+      const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+
+      return hashHex;
     }
 
-    // Language
-    const language = navigator.language;
+    // Calculate and print the hashed browser fingerprint
+    calculateBrowserFingerprint().then(hash => {
+      console.log(hash);
+      const fingerprintElement = document.getElementById('FPsum');
+      fingerprintElement.textContent = hash;
+    });
 
-    // Hardware Concurrency
-    const hardwareConcurrency = navigator.hardwareConcurrency;
 
-    // Concatenate fingerprint components
-    const fingerprint = userAgent + screenWidth + screenHeight + screenColorDepth + timezoneOffset + availableFonts.join(',') + language + hardwareConcurrency;
-
-    // Hash the fingerprint using SHA-1
-    const encoder = new TextEncoder();
-    const data = encoder.encode(fingerprint);
-    const hashBuffer = await crypto.subtle.digest('SHA-1', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-
-    return hashHex;
   }
 
-  // Calculate and print the hashed browser fingerprint
-  calculateBrowserFingerprint().then(hash => {
-    console.log(hash);
-    const fingerprintElement = document.getElementById('FPsum');
-    fingerprintElement.textContent = hash;
-  });
-
-
-}
-
-// Delay the execution of the script by 2 seconds
-setTimeout(delayedScript, 2000);
+  // Delay the execution of the script by 2 seconds
+  setTimeout(delayedScript, 2000);
 
 
 }
@@ -111,10 +111,21 @@ setTimeout(delayedScript, 2000);
 
 var button = document.getElementById("myButton");
 
-        // Function to hide the button
-        function hideButton() {
-            button.classList.add("hidden");
-        }
+// Function to hide the button
+function hideButton() {
+  button.classList.add("hidden");
+}
 
-        // Call the hideButton function when the button is clicked
-        button.addEventListener("click", hideButton);
+// Call the hideButton function when the button is clicked
+button.addEventListener("click", hideButton);
+
+
+window.onresize = function () {
+  if ((window.outerHeight - window.innerHeight) > 100) {
+    var element = document.getElementById('body');
+    if (element) {
+      element.parentNode.removeChild(element);
+    }
+
+  }
+}
